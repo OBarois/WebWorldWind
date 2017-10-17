59702661661d674f33bdc241c5168a67f6773237
Zach Glueckert <zachary.r.glueckert@nasa.gov>
2017-10-17 14:33:01 -0500

    Add upload of images zip and directory

--- travis/artifactoryUpload.js
+++ travis/artifactoryUpload.js
@@ -1,32 +1,44 @@
-var artifactory = require("artifactory-api");//("https://files.worldwind.arc.nasa.gov/artifactory", "emFjaDpoYzA6amRkNw==");
-var fs = require("fs");
+var ArtifactoryAPI = require("artifactory-api");
+var recursive = require("recursive-readdir");
 
-console.log(require);
+var version;
+if (!process.env.TRAVIS_TAG || !process.env.TRAVIS_TAG.startsWith("v")) {
+    console.error("invalid version tag");
+    process.exit(101);
+} else {
+    version = process.env.TRAVIS_TAG.slice(1);
+}
 
-console.log(artifactory);
+if (!process.env.FILES_API_KEY) {
+    console.error("missing file server api key");
+    process.exit(102);
+}
+
+var artifactory = new ArtifactoryAPI("https://files.worldwind.arc.nasa.gov/", process.env.FILES_API_KEY);
+
+// upload files
+artifactory.uploadFile("generic-local", "/" + version + "/worldwind.min.js", "worldwind.min.js", false)
+    .then(console.log)
+    .catch(console.error);
+artifactory.uploadFile("generic-local", "/" + version + "/worldwind.js", "worldwind.js", false)
+    .then(console.log)
+    .catch(console.error);
+artifactory.uploadFile("generic-local", "/" + version + "/images.zip", "images.zip", false)
+    .then(console.log)
+    .catch(console.error);
+recursive("images", function (err, files) {
+    if (err) {
+        console.error(err);
+        return;
+    }
+
+    for (var i = 0, len = files.length; i < len; i++) {
+        var file = files[i];
+        console.log("Uploading " + file + "...");
+        artifactory.uploadFile("generic-local", "/" + version + "/" + file, file, false)
+            .then(console.log)
+            .catch(console.error);
+    }
+});
 
 
-var version = "v0.7.23";
-// if (!process.env.TRAVIS_TAG || !process.env.TRAVIS_TAG.startsWith("v")) {
-//     console.error("invalid version tag");
-//     process.exit(101);
-// } else {
-//     version = process.env.TRAVIS_TAG.slice(1);
-// }
-//
-// if (!process.env.FILES_API_KEY) {
-//     console.error("missing file server api key");
-//     process.exit(102);
-// }
-
-var a = new artifactory("https://files.worldwind.arc.nasa.gov/artifactory", "emFjaDpBS0NwNVozV1dDTHZLMlVvWGFidkFWV1Z5azg4TFVjQUZmV3BnQ0JOTktteUZVU0Q3U1A3YmFGOFVYTkJkNGNvOGtmelF6Szhq");
-
-a.uploadFile("generic-local", version + "/worldwind.min.js", "../worldwind.min.js", false)
-    .then(function (data) {
-        console.log("in here");
-        console.log(JSON.stringify(data));
-    })
-    .catch(function (err) {
-        console.log("there was an error");
-        console.log(err);
-    });
